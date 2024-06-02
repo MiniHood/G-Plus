4b67e23ce7e8782e9515697d20564e2b23f120f4
HowNiceOfYou <75768388+MiniHood@users.noreply.github.com>
2024-06-02 08:08:21 -0600

    Add files via upload

---
+++ Needed Lua/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'lua52'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://sourceforge.net/projects/luabinaries/files/5.2.4/Tools%20Executables/lua-5.2.4_Win32_bin.zip/download'
+$url64      = 'http://sourceforge.net/projects/luabinaries/files/5.2.4/Tools%20Executables/lua-5.2.4_Win64_bin.zip/download'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+
+$pathToInstall = "$ENV:ChocolateyInstall\lib\$packageName\tools"
+$pathType = 'User'
+
+if ( ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+	[Security.Principal.WindowsBuiltInRole] "Administrator") ) {
+	$pathType = 'Machine'
+}
+
+Install-ChocolateyPath $pathToInstall $pathType
