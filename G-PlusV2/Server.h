d17d00feb3f09a6b67632965d0a38be8b488f940
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-27 23:56:09 +0100

    Removed server constructer

--- G-PlusV2/Server.h
+++ G-PlusV2/Server.h
@@ -5,19 +5,8 @@
 using namespace std;
 
 class Server {
-private:
-	string _servername;
-	string _ip;
-	int _port;
-
 public:
-	string servername() const { return _servername;  }
-	string ip() const { return _ip; }
-	int port() const { return _port; }
-
-	Server(string ip, int port, OPTIONAL string servername) {
-		_servername = servername;
-		_ip = ip;
-		_port = port;
-	}
+	string servername;
+	string ip;
+	int port;
 };
