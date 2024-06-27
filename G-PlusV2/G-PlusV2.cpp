de1d29ab8e2d6a9bf1c679c66a5f4e2cb2ff722a
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-27 23:56:17 +0100

    Setting up main

--- G-PlusV2/G-PlusV2.cpp
+++ G-PlusV2/G-PlusV2.cpp
@@ -2,10 +2,22 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-int main(int argc, char*argv[])
+void typewrite(string input)
 {
-    
+    for (const auto c : input) {
+        std::cout << c << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	// Display Credits
+    typewrite("This tool is free, if you paid for this you were scammed. Created by minihood.\nGithub: https://github.com/MiniHood/G-Plus/tree/v2");
 }
