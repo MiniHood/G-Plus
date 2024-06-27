b5bec4d00e438cb5ee08ed00dc6e66c25b926e27
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-27 21:32:27 +0100

    Added constructer to Client

--- G-PlusV2/Client.h
+++ G-PlusV2/Client.h
@@ -9,14 +9,24 @@
 using namespace std;
 
 class Client {
+
+private:
+	Server _server;
+	Steam _steam;
+	GMOD _gmod;
+
 public:
 	string username;
 	string password;
 	Server server() const { return _server; }
 	Steam steam() const { return _steam; }
 	GMOD gmod() const { return _gmod; }
-private:
-	Server _server;
-	Steam _steam;
-	GMOD _gmod;
+
+	Client(string client_username, string client_password, Server client_server, Steam client_steam, GMOD client_gmod) {
+		username = client_username;
+		password = client_password;
+		_server = client_server;
+		_steam = client_steam;
+		_gmod = client_gmod;
+	}
 };
