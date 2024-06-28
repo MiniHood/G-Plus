98fc106a7724d1495913ea8d0b87a17cce011e59
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-28 22:06:26 +0100

    Fixing class redefinition error

--- G-PlusV2/pages.cpp
+++ G-PlusV2/pages.cpp
@@ -1,35 +1,29 @@
+#pragma once
+
 #include "pages.h"
-#include <iostream>
 
 using namespace std;
 
-class pages {
-public:
-    class client {
-    public:
-        void mainmenu() {
-            cout << "[1] List active clients." << endl;
-            cout << "[2] Search client by username." << endl;
-            cout << "[3] Add new client." << endl;
 
-            cout << "[000] Terminate all clients." << endl;
-        }
-    };
-    class server {
-    public:
-        void mainmenu() {
-            cout << "[1] List saved servers." << endl;
-            cout << "[2] Search server by name." << endl;
-            cout << "[3] Add new server." << endl;
+    void pages::client::mainmenu() {
+        cout << "[1] List active clients." << endl;
+        cout << "[2] Search client by username." << endl;
+        cout << "[3] Add new client." << endl;
 
-            cout << "[000] Delete all server records." << endl;
-        }
-    };
-    class home {
-    public:
-        void mainmenu() {
-            cout << "[1] Servers" << endl;
-            cout << "[2] Clients" << endl;
-        }
-    };
-};
+        cout << endl << "[000] Terminate all clients." << endl;
+    }
+
+    void pages::server::mainmenu() {
+        cout << "[1] List saved servers." << endl;
+        cout << "[2] Search server by name." << endl;
+        cout << "[3] Add new server." << endl;
+           
+        cout << endl << "[000] Delete all server records." << endl;
+    }
+
+    void pages::home::mainmenu() {
+        cout << "[1] Servers" << endl;
+        cout << "[2] Clients" << endl;
+    }
+    
+   
--- G-PlusV2/pages.h
+++ G-PlusV2/pages.h
@@ -1,5 +1,7 @@
-#pragma once
+#include <iostream>
 
+#ifndef PAGES_AAA
+#define PAGES_AAA
 
 class pages {
 public:
@@ -17,3 +19,4 @@ public:
 	};
 };
 
+#endif
