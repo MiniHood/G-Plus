5722afcc5c6b997f8c57c090c9524b01c74c32ac
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-28 21:43:58 +0100

    Attempting to make a page system.
    Please don't ask, I can't think of another cleaner way of doing this.

--- G-PlusV2/G-PlusV2.cpp
+++ G-PlusV2/G-PlusV2.cpp
@@ -4,20 +4,38 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "GUI.h"
+#include "Memory.h"
+#include "pages.h"
 
 using namespace std;
+
+int currentpage;
+
+void clear_console() { system("cls"); }
+
+void sleep(int ms) { std::this_thread::sleep_for(chrono::milliseconds(ms)); }
 
 void typewrite(string input)
 {
     for (const auto c : input) {
-        std::cout << c << std::flush;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << c << flush;
+        sleep(5);
     }
     std::cout << std::endl;
 }
 
+
 int main(int argc, char* argv[])
 {
-	// Display Credits
-    typewrite("This tool is free, if you paid for this you were scammed. Created by minihood.\nGithub: https://github.com/MiniHood/G-Plus/tree/v2");
+    #ifdef NDEBUG #else // Just skipping if I'm debugging, can't be bothered to wait
+	    // Display Credits
+        string Warning = "This tool is free, if you paid for this you were scammed. \nGithub: https://github.com/MiniHood/G-Plus/tree/v2";
+        typewrite(Warning);
+        sleep(2000);
+        clear_console();
+    #endif
+
+    pages::home home;
+    home.mainmenu();
 }
---
+++ G-PlusV2/pages.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+class pages {
+public:
+	class client {
+	public:
+		void mainmenu();
+	};
+	class server {
+	public:
+		void mainmenu();
+	};
+	class home {
+	public:
+		void mainmenu();
+	};
+};
+
