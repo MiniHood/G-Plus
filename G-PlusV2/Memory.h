e1b1047544a73dc37d51c6913e3a65f8a29c7ddb
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-28 05:28:23 +0100

    Removed Q_ReadProcessMemory & Q_WriteProcessMemory

--- G-PlusV2/Memory.h
+++ G-PlusV2/Memory.h
@@ -4,5 +4,3 @@
 #include "Windows.h"
 #include "variant"
 
-variant<int, string, char> Q_WriteProcessMemory(variant<int, string, char> input, int process_id, LPVOID lpBaseAddress);
-variant<int, string, char> Q_ReadProcessMemory(int process_id, variant<LPCVOID> lpBaseAddress, variant<SIZE_T> size);
