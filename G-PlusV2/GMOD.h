61de4b6e5b1750e4e0df9368fd16498061398959
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-29 01:25:26 +0100

    Setting up GMOD memory

--- G-PlusV2/GMOD.h
+++ G-PlusV2/GMOD.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <tchar.h>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
 
 class GMOD {
-	HANDLE Process;
-
-	struct Offsets {
-
-	};
+public:
+	HANDLE pHandle;
+	// Memory management
+	DWORD GetModuleBaseAddress(TCHAR* lpszModuleName);
+	DWORD GetPointerAddress(DWORD gameBaseAddr, DWORD address, vector<DWORD> offsets);
 };
