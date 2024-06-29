d47a6435d114a747235fd99c5067a1bd05f9cb83
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-29 01:42:58 +0100

    Added GetServerConnection()

--- G-PlusV2/GMOD.cpp
+++ G-PlusV2/GMOD.cpp
@@ -1,41 +1,31 @@
 #include "GMOD.h"
 
-DWORD GMOD::GetModuleBaseAddress(TCHAR* lpszModuleName) {
-    DWORD dwModuleBaseAddress = 0;
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetProcessId(this->pHandle)); // make snapshot of all modules within process
-    MODULEENTRY32 ModuleEntry32 = { 0 };
-    ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
-
-    if (Module32First(hSnapshot, &ModuleEntry32)) //store first Module in ModuleEntry32
-    {
-        do {
-            if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0) // if Found Module matches Module we look for -> done!
-            {
-                dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
-                break;
-            }
-        } while (Module32Next(hSnapshot, &ModuleEntry32)); // go through Module entries in Snapshot and store in ModuleEntry32
-
-
-    }
-    CloseHandle(hSnapshot);
-    return dwModuleBaseAddress;
-}
-
-DWORD GMOD::GetPointerAddress(DWORD gameBaseAddr, DWORD address, vector<DWORD> offsets)
+string GMOD::GetServerConnection()
 {
-    DWORD pID = NULL; // Game process ID
-    GetWindowThreadProcessId(FindWindowA(NULL, "Garry\'s Mod (x64)"), &pID);
-    HANDLE phandle = NULL;
-    phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
-    if (phandle == INVALID_HANDLE_VALUE || phandle == NULL);
+    // TODO: When free change this to signature scanning.
+    
+    char vstModule[] = "vstdlib.dll";
+    // Status
+    // CT = no connection in menu
+    // NULL or url means in local game
+    // In an actual game will show ip:port
+    static DWORD CurrentConnectionAddress = 0x007DBE68;
+    static vector<DWORD> CurrentConnectionOffsets{ 0x20, 0x10, 0x50, 0x10, 0x38, 0x48, 0xF8 };
 
-    DWORD offset_null = NULL;
-    ReadProcessMemory(phandle, (LPVOID*)(gameBaseAddr + address), &offset_null, sizeof(offset_null), 0);
-    DWORD pointeraddress = offset_null; // the address we need
-    for (int i = 0; i < offsets.size() - 1; i++) // we dont want to change the last offset value so we do -1
+    if (this->pHandle == NULL)
+        return;
+
+    DWORD BaseAddress = this->GetModuleBaseAddress(vstModule);
+    DWORD ConnectionPointerAddress = GetPointerAddress(BaseAddress, CurrentConnectionAddress, CurrentConnectionOffsets);
+
+    string ConnectionBuffer;
+    BOOL ReadStatus = ReadProcessMemory(this->pHandle, (LPVOID*)ConnectionPointerAddress, &ConnectionBuffer, 20, NULL);
+
+    if (ReadStatus == false)
     {
-        ReadProcessMemory(phandle, (LPVOID*)(pointeraddress + offsets.at(i)), &pointeraddress, sizeof(pointeraddress), 0);
+        cout << "Error reading current server connection. Error code: " << GetLastError() << endl;
+        return;
     }
-    return pointeraddress += offsets.at(offsets.size() - 1); // adding the last offset
+        
+    return ConnectionBuffer;
 }
