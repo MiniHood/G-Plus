9dbfde6d79f4ddcdbfb1af5280bc614a0349625c
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-30 06:33:16 +0100

    Reverted declaration on AddNewClient & RemoveClient

--- G-PlusV2/Controller.h
+++ G-PlusV2/Controller.h
@@ -8,14 +8,24 @@
 
 
 namespace Controller {
-	// dont bully me for using externs
 	inline std::set<Client*> Clients;
 	inline std::set<Server*> Servers;
 	
 	// if this doesnt work then we'll use OUT set<Client*> outPtr
-	static void AddNewClient(Client* client);
+	static void AddNewClient(Client* client) {
+		if (Clients.contains(client)) {
+			return;
+		}
 
-	static void RemoveClient(Client* client);
+		Clients.insert(client);
+		std::cout << Clients.size() << std::endl;
+	}
+
+	static void RemoveClient(Client* client)
+	{
+		auto i = find(Clients.begin(), Clients.end(), client);
+		Clients.erase(i);
+	}
 }
 
 #endif
