d382ec37e66d962f6e48b169e1c886cdb43a1cd8
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-30 06:35:00 +0100

    Added utils

---
+++ G-PlusV2/util.cpp
@@ -0,0 +1,36 @@
+#include "util.h"
+#include <Windows.h>
+
+static void util::clear_console()
+{
+    COORD topLeft = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
+}
+
+static void util::CoutCentered(std::string text)
+{
+    // This function will only center the text if it is less than the length of the console!
+    // Otherwise it will just display it on the console without centering it.
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Get the console handle.
+    PCONSOLE_SCREEN_BUFFER_INFO lpScreenInfo = new CONSOLE_SCREEN_BUFFER_INFO(); // Create a pointer to the Screen Info pointing to a temporal screen info.
+    GetConsoleScreenBufferInfo(hConsole, lpScreenInfo); // Saves the console screen info into the lpScreenInfo pointer.
+    COORD NewSBSize = lpScreenInfo->dwSize; // Gets the size of the screen
+    if (NewSBSize.X > text.size()) {
+        int newpos = ((NewSBSize.X - text.size()) / 2); // Calculate the number of spaces to center the specific text.
+        for (int i = 0; i < newpos; i++) std::cout << " "; // Prints the spaces
+    }
+    std::cout << text << std::endl; // Prints the text centered :]
+}
---
+++ G-PlusV2/util.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "Windows.h"
+
+namespace util {
+    static void clear_console();
+
+    static void CoutCentered(std::string text);
+}
