f748e8671f6f5eb64ea8a20f141d2b33e6ac3f62
HowNiceOfYou <smellywelly1221@gmail.com>
2024-06-30 08:19:21 +0100

    Fixed launchExecutable and changed name

--- G-PlusV2/Steam.cpp
+++ G-PlusV2/Steam.cpp
@@ -2,7 +2,7 @@
 #include "Globals.h"
 #include "Client.h"
 
-
+using namespace std;
 
 HANDLE Steam::StartSteam(Client* client) {
 	// Private called by Load()
@@ -31,21 +31,21 @@ bool Steam::StopSteam() {
 
 bool Steam::_HasUpdate() // Needs fixed
 {
-	std::string path = Globals::Steam::Path;
-	std::string steam_client_win32 = path.append("package\\steam_client_win32.manifest");
-	std::cout << steam_client_win32 << std::endl;
-	std::ifstream File(steam_client_win32);
-	std::string lineBuffer;
+	string path = Globals::Steam::Path;
+	string steam_client_win32 = path.append("package\\steam_client_win32.manifest");
+	cout << steam_client_win32 << endl;
+	ifstream File(steam_client_win32);
+	string lineBuffer;
 
 	if (!File.is_open())
 		this->_HasUpdate(); // Just keep retrying until we get it.
 
 	while (getline(File, lineBuffer))
 	{
-		if (lineBuffer.find("version") != std::string::npos) // If we're on the line with version then
+		if (lineBuffer.find("version") != string::npos) // If we're on the line with version then
 		{
 			// Check if the line contains our current version
-			if (lineBuffer.find(Globals::Steam::Version) != std::string::npos)
+			if (lineBuffer.find(Globals::Steam::Version) != string::npos)
 				return false;
 		}
 	}
@@ -68,73 +68,60 @@ void Steam::_ForceUpdate(Client* client)
 #include <windows.h>
 #include <iostream>
 
-PROCESS_INFORMATION launchExecutable(const std::string& programPath, const std::string& arguments) {
-	STARTUPINFO sInfo;
-	PROCESS_INFORMATION pInfo;
+PROCESS_INFORMATION _Launch(const string& programPath, const string arguments) {
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi; // The function returns this
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
 
-	ZeroMemory(&sInfo, sizeof(sInfo));
-	sInfo.cb = sizeof(sInfo);
-	ZeroMemory(&pInfo, sizeof(pInfo));
+	cout << programPath << endl;
 
-	// Convert strings to LPCTSTR format required by CreateProcess
-	LPCTSTR lpApplicationName = programPath.c_str();
-	LPCTSTR lpCommandLine = arguments.c_str();
-
-	BOOL success = CreateProcess(lpApplicationName,   // Application name
-	   (LPSTR)lpCommandLine,       // Command line arguments
-		NULL,                // Process handle not inheritable
-		NULL,                // Thread handle not inheritable
-		FALSE,               // Set handle inheritance to FALSE
-		CREATE_NO_WINDOW,    // Create No Window flag
-		NULL,                // Use parent's environment block
-		NULL,                // Use parent's starting directory
-		&sInfo,              // Pointer to STARTUPINFO structure
-		&pInfo);             // Pointer to PROCESS_INFORMATION structure
-
-	if (!success) {
-		DWORD dwError = GetLastError();
-		std::cout << "CreateProcess failed (" << dwError << ").\n";
-		return pInfo;
+	// Start the child process.
+	if (!CreateProcessA(
+		programPath.c_str(),      // app path
+		(LPSTR)arguments.c_str(),     // Command line 
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0, // No window creation flag
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)           // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		cout << "Failed to create steam process" << endl;
+		return;
 	}
-	else {
-		return pInfo;
+	else
+	{
+		return pi;
 	}
 }
 
 
-HANDLE Steam::_StartSteamApplication(Client* client, std::string ipc_name) {
+HANDLE Steam::_StartSteamApplication(Client* client, string ipc_name) {
 	// Private called by StartSteam()
 	// TODO: Add steam guard support
 
-	std::ostringstream cmdStream;
+	ostringstream cmdStream;
+	ostringstream steamPath;
+	steamPath << Globals::Steam::Path << "steam.exe";
+
 	cmdStream <<  // Set project enviroment variable
-		Globals::Steam::Path << // Give path to start
-		"steam.exe \" " << // Add steam.exe to path
+		steamPath.str() << // Give path to start
+		" " << // Add steam.exe to path
 		"-master_ipc_name_override " << // Set steam to different shared memory
 		ipc_name << // pass our ipc name
-		" -login " << // Make sure steam logs us in
+		"-login " << // Make sure steam logs us in
 		client->username << // Pass our username
 		" " << // Whitespace
 		client->password; // Pass our password
 
-	PROCESS_INFORMATION cmd = launchExecutable("C:\WINDOWS\system32\cmd.exe", "set VPROJECT=gplus");
+	PROCESS_INFORMATION cmd = _Launch(steamPath.str(), cmdStream.str());
 
-	std::ostringstream SteamString;
-	SteamString << Globals::Steam::Path << "steam.exe";
+	// Make sure to close handle on the thread and handle later
 
-	std::ostringstream SteamArguments;
-	SteamArguments 
-		<< "-master_ipc_name_override " << // Set steam to different shared memory
-		ipc_name << // pass our ipc name
-		" -login " << // Make sure steam logs us in
-		client->username << // Pass our username
-		" " << // Whitespace
-		client->password; // Pass our password
-
-	PROCESS_INFORMATION steam = launchExecutable(SteamString.str(), SteamArguments.str());
-
-	CloseHandle(cmd.hProcess);
-	CloseHandle(cmd.hThread);
-
-	return steam.hProcess;
+	return cmd.hProcess;
 }
