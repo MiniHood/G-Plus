716a785d5a6778055835b29c478cde36c5edfa61
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-03 16:24:44 +0100

    Added 20 new themes.

---
+++ G-PlusV2/Styles.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace Styles {
+	void BlackDevil();
+	void BootstrapDark();
+	void Cherry();
+	void Classic();
+	void Classic_Steam();
+	void Clean_Dark_Red();
+	void Comfy();
+	void Duck_Quack_Red();
+	void Darcula();
+	void Dark();
+	void Dark_Ruda();
+	void Darky();
+	void DeepDark();
+	void Discord_Dark();
+	void Enemyouse();
+	void Future_Dark();
+	void Gold();
+	void GreenFont();
+	void Light();
+	void Material_Flat();
+	void Microsoft();
+	void Modern();
+	void Moonlight();
+	void Photoshop();
+	void Purple_Comfy();
+	void Quick_Minimal_Look();
+	void Red_Font();
+	void Red_Oni();
+	void Rounded_Visual_Studio();
+	void Soft_Cherry();
+	void Sonic_Riders();
+	void Unreal();
+	void Visual_Studio();
+	void IedSynthmaster();
+}
