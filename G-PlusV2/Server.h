9d713b688a6f926167c0179e24c7771b89d8a502
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-03 14:35:49 +0100

    Fixed formatting in server.h

--- G-PlusV2/Server.h
+++ G-PlusV2/Server.h
@@ -7,11 +7,10 @@
 class Server {
 public:
 	std::string servername = "";
-	const char* wservername;
 	std::string ip;
 	std::string port;
 
-		Server(std::string server_name,  std::string _ip, std::string _port)
+	Server(std::string server_name, std::string _ip, std::string _port)
 	{
 		servername = server_name;
 		ip = _ip;
