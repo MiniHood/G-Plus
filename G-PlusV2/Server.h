69b2f6dc702ba7700f915f07cf6df014b32ce801
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-03 12:21:28 +0100

    Added constructer to server class

--- G-PlusV2/Server.h
+++ G-PlusV2/Server.h
@@ -3,9 +3,18 @@
 #include <iostream>
 
 
+
 class Server {
 public:
-	std::string servername;
+	std::string servername = "";
+	const char* wservername;
 	std::string ip;
-	int port;
+	std::string port;
+
+		Server(std::string server_name,  std::string _ip, std::string _port)
+	{
+		servername = server_name;
+		ip = _ip;
+		port = _port;
+	}
 };
