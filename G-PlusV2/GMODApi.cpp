94c2dfbfaf5d31a55a16e0b36f9650a0b518da15
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-03 16:29:54 +0100

    Setting idea for GMODApi.cpp

---
+++ G-PlusV2/GMODApi.cpp
@@ -0,0 +1,8 @@
+/*
+
+Functions will include;
+hooking functions for injecting our DLL
+Communication functions to pass our .lua files to the dll
+Function should not immediatly execute until told.
+
+*/
