52125010c2f4f458ec95f1b780f651425655b627
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-04 23:55:13 +0100

    Changed const globals to static

--- G-PlusV2/Globals.h
+++ G-PlusV2/Globals.h
@@ -3,11 +3,11 @@
 
 namespace Globals {
 	namespace Steam {
-		const std::string Path = "C:\\Program Files (x86)\\Steam\\";
-		const int Version = 1718904662;
+		static std::string Path = "C:\\Program Files (x86)\\Steam\\";
+		static int Version = 1718904662;
 	}
 	namespace GMOD {
-		const std::string Path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod\\";
-		const std::string PatchVersion = "2023.06.28";
+		static std::string Path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod\\";
+		static std::string PatchVersion = "2023.06.28";
 	}
 }
