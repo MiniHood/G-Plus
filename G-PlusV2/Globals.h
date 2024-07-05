eed3e1c75c39d3afa956ee3f5458931e92e2e352
HowNiceOfYou <smellywelly1221@gmail.com>
2024-07-05 11:52:37 +0100

    Added searchtimeout global

--- G-PlusV2/Globals.h
+++ G-PlusV2/Globals.h
@@ -9,5 +9,7 @@ namespace Globals {
 	namespace GMOD {
 		static std::string Path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod\\";
 		static std::string PatchVersion = "2023.06.28";
+		// If your game hasnt started by then, we can safely assume it's not going too and we're hogging resources by searching for it. After it destroys the class and closes steam.
+		static int SearchTimeOut = 10000; 
 	}
 }
